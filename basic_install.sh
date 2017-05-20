c02a1476651570208a83204a5f0c526f612e299f
marconimp <marconimp@yahoo.com.br>
2017-05-20 17:10:42 -0300

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -8,25 +8,25 @@
 ####
 ##############################################################################
 
-### Add needed repositories ###
+#Add needed repositories
 sudo echo 'Add needed repositories'
 sudo add-apt-repository ppa:mc3man/trusty-media
 ##############################################################################
-### Update ###
+# Update
 sudo echo 'Update packages information'
 sudo apt-get update
 ##############################################################################
-### Install packages ###
+# Install packages
 sudo echo 'Install the packages'
 sudo apt-get install apache2 apache2-utils php5 php5-cli php5-sqlite git ffmpeg vlc 
 ##############################################################################
-### Download Panel ###
+# Download Panel
 sudo echo 'Download the Panel'
 cd /var/www/
 git clone https://cbazone@bitbucket.org/cbazone/myiptv.git
 ##############################################################################
-### Configurations ###
-## Auth ##
+# Configurations
+# Auth
 sudo echo 'Configure the authorizations'
 sudo echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/vlc' >> /etc/sudoers
 sudo echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/cvlc' >> /etc/sudoers
@@ -35,7 +35,7 @@ cd /var/www/
 chown -R www-data myiptv
 chgrp -R www-data myiptv
 
-## Apache ##
+# Apache
 sudo echo 'Configure the Apache WebServer'
 cd /etc/apache2/sites-available/
 sudo cp 000-default.conf 000-default.conf_bak
@@ -43,7 +43,7 @@ cd /var/www/myiptv/install/000-default.conf /etc/apache2/sites-available/000-def
 sudo a2enmod rewrite
 sudo service apache2 restart
 
-## Panel ##
+# Panel
 sudo echo 'Configure the Panel'
 cd /var/www/myiptv/config
 sudo cp config_template.php config.php
